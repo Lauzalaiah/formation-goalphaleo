ace75ce6fdb03a49683fd88b7749152cc07a877f
Lauzalaiah <82446542+Lauzalaiah@users.noreply.github.com>
2026-05-11 22:30:36 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+    ]
   },
 }
 
