660ee1902c0a17e96e81dc7a16ea536324761e6e
Lauzalaiah <82446542+Lauzalaiah@users.noreply.github.com>
2026-05-13 12:05:41 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,47 +1,4 @@
-import { createServerClient } from "@supabase/ssr"
-import { NextResponse, type NextRequest } from "next/server"
-
-export async function middleware(req: NextRequest) {
-  let response = NextResponse.next()
-
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return req.cookies.get(name)?.value
-        },
-
-        set(name: string, value: string, options: any) {
-          response.cookies.set({
-            name,
-            value,
-            ...options,
-          })
-        },
-
-        remove(name: string, options: any) {
-          response.cookies.set({
-            name,
-            value: "",
-            ...options,
-          })
-        },
-      },
-    }
-  )
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser()
-
-  if (!user && req.nextUrl.pathname.startsWith("/formation")) {
-    return NextResponse.redirect(new URL("/auth/login", req.url))
-  }
-
-  return response
-}
+export function middleware() {}
 
 export const config = {
   matcher: ["/formation/:path*"],
