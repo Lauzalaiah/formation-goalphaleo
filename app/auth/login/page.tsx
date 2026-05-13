592a9d2d515617f4ad174f386bef3785ce65c9c1
Lauzalaiah <82446542+Lauzalaiah@users.noreply.github.com>
2026-05-13 17:36:53 +0300

    Update page.tsx

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -91,7 +91,7 @@ export default function LoginPage() {
 
       console.log("REDIRECT TO FORMATION")
 
-      window.location.replace("/formation")
+     window.location.assign("/formation")
 
     } catch (err) {
       console.log("GLOBAL ERROR", err)
